b43584872436ebfab23708331151c1e462530ac4
117503445 <t117503445@gmail.com>
2017-06-24 13:41:24 +0800

    Demo06241341

---
+++ FrmMain.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace CheckIn
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxDay = new System.Windows.Forms.ComboBox();
+            this.comboBoxTime = new System.Windows.Forms.ComboBox();
+            this.comboBoxType = new System.Windows.Forms.ComboBox();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonEnd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBoxDay
+            // 
+            this.comboBoxDay.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBoxDay.FormattingEnabled = true;
+            this.comboBoxDay.Items.AddRange(new object[] {
+            "周一",
+            "周二",
+            "周三",
+            "周四",
+            "周五",
+            "周六",
+            "周七"});
+            this.comboBoxDay.Location = new System.Drawing.Point(73, 470);
+            this.comboBoxDay.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBoxDay.Name = "comboBoxDay";
+            this.comboBoxDay.Size = new System.Drawing.Size(56, 25);
+            this.comboBoxDay.TabIndex = 0;
+            this.comboBoxDay.Text = "周一";
+            // 
+            // comboBoxTime
+            // 
+            this.comboBoxTime.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBoxTime.FormattingEnabled = true;
+            this.comboBoxTime.Items.AddRange(new object[] {
+            "早",
+            "午",
+            "晚"});
+            this.comboBoxTime.Location = new System.Drawing.Point(135, 470);
+            this.comboBoxTime.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBoxTime.Name = "comboBoxTime";
+            this.comboBoxTime.Size = new System.Drawing.Size(38, 25);
+            this.comboBoxTime.TabIndex = 1;
+            this.comboBoxTime.Text = "早";
+            this.comboBoxTime.SelectedIndexChanged += new System.EventHandler(this.comboBoxTime_SelectedIndexChanged);
+            // 
+            // comboBoxType
+            // 
+            this.comboBoxType.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBoxType.FormattingEnabled = true;
+            this.comboBoxType.Location = new System.Drawing.Point(179, 470);
+            this.comboBoxType.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBoxType.Name = "comboBoxType";
+            this.comboBoxType.Size = new System.Drawing.Size(38, 25);
+            this.comboBoxType.TabIndex = 2;
+            this.comboBoxType.Text = "读";
+            // 
+            // listBox
+            // 
+            this.listBox.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 17;
+            this.listBox.Location = new System.Drawing.Point(800, 13);
+            this.listBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(230, 633);
+            this.listBox.TabIndex = 3;
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonLoad.Location = new System.Drawing.Point(494, 448);
+            this.buttonLoad.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(131, 67);
+            this.buttonLoad.TabIndex = 4;
+            this.buttonLoad.Text = "读取";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonSave.Location = new System.Drawing.Point(631, 448);
+            this.buttonSave.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(131, 67);
+            this.buttonSave.TabIndex = 5;
+            this.buttonSave.Text = "保存";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonEnd
+            // 
+            this.buttonEnd.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonEnd.Location = new System.Drawing.Point(631, 529);
+            this.buttonEnd.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonEnd.Name = "buttonEnd";
+            this.buttonEnd.Size = new System.Drawing.Size(131, 67);
+            this.buttonEnd.TabIndex = 6;
+            this.buttonEnd.Text = "退出";
+            this.buttonEnd.UseVisualStyleBackColor = true;
+            this.buttonEnd.Click += new System.EventHandler(this.buttonEnd_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1036, 661);
+            this.Controls.Add(this.buttonEnd);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.comboBoxType);
+            this.Controls.Add(this.comboBoxTime);
+            this.Controls.Add(this.comboBoxDay);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FrmMain";
+            this.Text = "CheckIn";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxDay;
+        private System.Windows.Forms.ComboBox comboBoxTime;
+        private System.Windows.Forms.ComboBox comboBoxType;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonEnd;
+    }
+}
+
---
+++ FrmMain.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckIn
+{
+    public partial class FrmMain : Form
+    {
+        public string[] studentData;
+
+        public string[] studentName = new string[89];//姓名
+        public int[] studentID = new int[89];//学号
+        public bool[] studentQd = new bool[89];//签到
+        public string filedsName = "";//例如 周四早眼
+        public int[] studentColor = new int[89];//0 为白色，1为红色，2为灰色
+        public Button[] btnStudents = new Button[89];//学生们
+        public FrmMain()
+        {
+            InitializeComponent();
+            #region 初始化
+            for (int i = 0; i < 89; i++) { studentQd[i] = true; btnStudents[i] = new Button(); this.Controls.Add(btnStudents[i]); btnStudents[i].Visible = false;
+                btnStudents[i].BackColor = Color.AliceBlue;
+            }
+            #endregion
+            #region LoadStudent
+            int Top = 0;
+            int left = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                Top = 0;
+
+                for (int j = 0; j < 9; j++)
+                {
+                    btnStudents[10 * i + j].Top = Top;
+                    btnStudents[10 * i + j].Left = left;
+                    btnStudents[10 * i + j].FlatStyle = System.Windows.Forms.FlatStyle.Popup;//Style
+                    btnStudents[10 * i + j].Click += new System.EventHandler(this.btnStudents_Click);//注册单击事件
+                    Top += 30;
+                }
+                left += 80;
+            }
+            #endregion
+
+            #region 读取students.txt
+            studentData = System.IO.File.ReadAllLines(System.Environment.CurrentDirectory + "/file/txt/students.txt", Encoding.Default);
+            System.Console.WriteLine("读取students.txt");
+            foreach (string i in studentData)
+            {
+                Console.WriteLine(i);
+                int index = Int32.Parse(i.Substring(0, 2));
+                studentName[index] = i.Substring(2, 4);
+                studentID[index] = Int32.Parse(i.Substring(6, 2));
+                btnStudents[index].Text = studentName[index];
+                btnStudents[index].Visible = true;
+            }
+            #endregion
+            #region 填充comboBox
+            string weekstr = DateTime.Now.DayOfWeek.ToString();
+            switch (weekstr)
+            {
+                case "Monday": comboBoxDay.Text = "周一"; break;
+                case "Tuesday": comboBoxDay.Text = "周二"; break;
+                case "Wednesday": comboBoxDay.Text = "周三"; break;
+                case "Thursday": comboBoxDay.Text = "周四"; break;
+                case "Friday": comboBoxDay.Text = "周五"; break;
+                case "Saturday": comboBoxDay.Text = "周六"; break;
+                case "Sunday": comboBoxDay.Text = "周日"; break;
+            }
+            System.DateTime currentTime = new System.DateTime();
+            int hourint = currentTime.Hour;
+            switch (hourint)
+            {
+                case (6): comboBoxTime.Text = "早"; comboBoxType.Text = "读"; break;
+                case (10): comboBoxTime.Text = "早"; comboBoxType.Text = "眼"; break;
+                case (12): comboBoxTime.Text = "午"; comboBoxType.Text = "休"; break;
+                case (15): comboBoxTime.Text = "午"; comboBoxType.Text = "眼"; break;
+                case (5): comboBoxTime.Text = "晚"; comboBoxType.Text = "修"; break;
+                case (8): comboBoxTime.Text = "晚"; comboBoxType.Text = "眼"; break;
+                default: listBox.Items.Add("[Warn]奇怪的时间"); break;
+            }
+
+            #endregion
+
+        }
+
+        private void btnStudents_Click(object sender, System.EventArgs e)
+        {
+            String[] arrs = ((Button)sender).Text.Split('\n');
+            int ID = 0;
+            for (int i = 0; i < 89; i++)
+            {
+                if (arrs[0] == studentName[i]) { ID = i; break; }//查找学号
+            }
+            Console.WriteLine("当前位置:" + ID);
+            Console.WriteLine("当前颜色" + ((Button)sender).BackColor);
+            if (((Button)sender).BackColor == Color.Orange)
+            {
+                ((Button)sender).BackColor = Color.LightSteelBlue;
+                studentColor[ID] = 2;
+                studentQd[ID] = true;
+            }
+            else if (studentQd[ID] == true & ((Button)sender).BackColor == Color.LightSteelBlue)
+            {
+                ((Button)sender).BackColor = Color.AliceBlue;
+                studentColor[ID] = 0;
+            }
+            else
+            {
+                ((Button)sender).BackColor = Color.Orange;
+                studentColor[ID] = 1;
+                studentQd[ID] = false;
+            }
+            Console.WriteLine("当前签到情况" + studentQd[ID]);
+
+            string[] tempstr = new string[89];
+            for (int i = 0; i < 89; i++)
+            {
+                tempstr[i] = studentColor[i].ToString();
+            }
+            System.IO.File.WriteAllLines(System.Environment.CurrentDirectory + "/file/txt/temp.txt", tempstr);
+
+
+        }
+
+        private void comboBoxTime_SelectedIndexChanged(object sender, EventArgs e)
+
+
+        {
+            comboBoxType.Items.Clear();
+
+            switch (comboBoxTime.Text)
+            {
+
+                case "早": comboBoxType.Items.Add("读"); comboBoxType.Text = "读"; break;
+                case "午": comboBoxType.Items.Add("休"); comboBoxType.Text = "休"; break;
+                case "晚": comboBoxType.Items.Add("修"); comboBoxType.Text = "修"; break;
+
+            }
+
+            comboBoxType.Items.Add("眼");
+
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            filedsName = comboBoxDay.Text + comboBoxTime.Text + comboBoxType.Text;
+            Console.WriteLine("当前fieldsname" + filedsName);
+
+            listBox.Items.Clear();
+
+            for (int j = 0; j < 49; j++)
+            {
+                for (int i = 0; i < 89; i++)
+                {
+                    if (studentID[i] != 0 & !studentQd[i] & studentID[i] == j) { listBox.Items.Add(studentID[i] + studentName[i]); break; }
+                }
+            }
+        }
+
+        private void buttonEnd_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            string[] tempReceiver = System.IO.File.ReadAllLines(System.Environment.CurrentDirectory + "/file/txt/temp.txt", Encoding.Default);
+            for (int i = 0; i < 89; i++)
+            {
+                string[] tempstr = tempReceiver[i].Split(';');
+                Console.WriteLine(tempstr[0]);
+                switch (tempstr[0])
+                {
+                    case "0": btnStudents[i].BackColor = Color.AliceBlue; studentQd[i] = true; break;
+                    case "1": btnStudents[i].BackColor = Color.Orange; studentQd[i] = false; break;
+                    case "2": btnStudents[i].BackColor = Color.LightSteelBlue; studentQd[i] = true; break;
+                }
+
+            }
+
+        }
+    }
+
+}
