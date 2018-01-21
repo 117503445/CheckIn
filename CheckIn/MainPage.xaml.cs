5113e3d0a02f3501abc923233d0a311a5157f2bd
117503445 <t117503445@gmail.com>
2018-01-21 11:19:24 +0800

    demo0121_1129

--- CheckIn/MainPage.xaml.cs
+++ CheckIn/MainPage.xaml.cs
@@ -15,12 +15,11 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using System.Xml.Linq;
 using System.Threading.Tasks;
-using SQLite.Net.Attributes;
 using Windows.Storage;
-using SQLite.Net;
-using SQLite.Net.Platform.WinRT;
 using System.Text;
 using Windows.UI.Popups;
+using Windows.Storage.Streams;
+using Windows.Storage.Pickers;
 
 // https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
 
@@ -55,7 +54,7 @@ namespace CheckIn
             XElement xElement = XElement.Load(@"Assets\Student.xml");
             foreach (var item in xElement.Elements())
             {
-                Debug.WriteLine(item);
+                //Debug.WriteLine(item);
                 string name = item.Attribute("name").Value;
                 int id = int.Parse(item.Attribute("id").Value);
                 int row = int.Parse(item.Attribute("row").Value);
@@ -69,24 +68,55 @@ namespace CheckIn
             App.Current.Exit();
         }
 
-        private async void BtnSave_Click(object sender, RoutedEventArgs e)
+        private async void BtnSave_ClickAsync(object sender, RoutedEventArgs e)
         {
-            foreach (var item in stus)
+            //XElement xlog = new XElement("log");
+            //DateTime t = DateTime.Now;
+            //xlog.Add(new XAttribute("time", $"{0},{1},1,2"));
+            //xElement.Add(xlog);
+            //Debug.WriteLine(xElement);
+            //XDocument xDocument = new XDocument(xElement);
+            try
             {
-                Debug.WriteLine(item.Id);
-            }
-            using (var conn = AppDatabase.GetDbConnection())
-            {
-                StringBuilder msg = new StringBuilder();
-                var dbPerson = conn.Table<Demo>();
-                msg.AppendLine($"数据库中总共 {dbPerson.Count()} 个 Person 对象。");
-                foreach (var person in dbPerson)
-                {
-                    msg.AppendLine($"Id：{person.Id}；Name：{person.Name}");
-                }
+                Windows.Storage.StorageFolder storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+                Windows.Storage.StorageFile sampleFile = await storageFolder.CreateFileAsync("sample.xml",
+                    Windows.Storage.CreationCollisionOption.OpenIfExists);
 
-                await new MessageDialog(msg.ToString()).ShowAsync();
+                string text = await Windows.Storage.FileIO.ReadTextAsync(sampleFile);
+                Debug.WriteLine(text);
+                XDocument xDoc;//Debug.WriteLine();
+                try
+                {
+                    xDoc = XDocument.Load(text);
+                }
+                catch (Exception)
+                {
+                    xDoc = new XDocument();
+                    xDoc.Add(new XElement("Logs"));
+                    Debug.WriteLine(xDoc);
+                }
+                DateTime t = DateTime.Now;
+                xDoc.Element("Logs").Add(new XElement("Log", new XAttribute("time", 
+                    string.Format("{0},{1},{2},{3}",t.Month,t.Day,t.Hour,t.Minute,t.Second))));
+                var stream = await sampleFile.OpenAsync(Windows.Storage.FileAccessMode.ReadWrite);
+                using (var outputStream = stream.GetOutputStreamAt(0))
+                {
+                    using (var dataWriter = new Windows.Storage.Streams.DataWriter(outputStream))
+                    {
+                        dataWriter.WriteString(xDoc.ToString());
+                        await dataWriter.StoreAsync();
+                        await outputStream.FlushAsync();
+                    }
+                }
+                stream.Dispose(); // Or use the stream variable (see previous code snippet) with a using statement as well.
             }
+            catch (Exception ex)
+            {
+
+                Debug.WriteLine(ex);
+            }
+            
+
         }
     }
     public class Student
@@ -128,36 +158,5 @@ namespace CheckIn
         public int Id { get => id; set => id = value; }
         public int Row { get => row; set => row = value; }
         public int Column { get => column; set => column = value; }
-    }
-    public class Demo {
-        [PrimaryKey]// 主键。  
-        public int Id
-        {
-            get;
-            set;
-        }
-
-        public string Name
-        {
-            get;
-            set;
-        }
-    }
-    public static class AppDatabase
-    {
-        /// <summary>  
-        /// 数据库文件所在路径，这里使用 LocalFolder，数据库文件名叫 test.db。  
-        /// </summary>  
-        public readonly static string DbPath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "test.db");
-   
-        public static SQLiteConnection GetDbConnection()
-        {
-            Debug.WriteLine(DbPath);
-       // 连接数据库，如果数据库文件不存在则创建一个空数据库。  
-       var conn = new SQLiteConnection(new SQLitePlatformWinRT(), DbPath);
-            // 创建 Person 模型对应的表，如果已存在，则忽略该操作。  
-            conn.CreateTable<Demo>();
-            return conn;
-        }
     }
 }
---
+++ Demo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument xDoc;
+            xDoc = new XDocument(new XElement("Logs"));
+            // xDoc.Add(new XElement("Logs"));
+            xDoc.Save("1.xml");
+            Console.WriteLine(xDoc.Save);
+            Console.Read();
+        }
+    }
+}
