74052bec215208e1a9cb088327ee95d113c76055
117503445 <t117503445@gmail.com>
2018-02-07 21:09:20 +0800

    demo0207_2109

--- CheckIn/PageAdmin.xaml.cs
+++ CheckIn/PageAdmin.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
@@ -25,55 +26,140 @@ namespace CheckIn
     /// </summary>
     public sealed partial class PageAdmin : Page
     {
-        public  PageAdmin()
+        StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+        public PageAdmin()
         {
-            this.InitializeComponent();  
+            this.InitializeComponent();
+
+            DemoAsync();
             ShowGridViewItemOfStusAsync(App.Stus);
         }
-        private async Task CalculateStusScoreAsync()
+        private async void DemoAsync()
         {
-            StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+            try
+            {
+                var i = await GetFiles();
+                CboFile.ItemsSource = i;
+            }
+            catch (Exception)
+            {
+
+
+            }
+
+        }
+
+        private async Task<List<string>> GetFiles()
+        {
+            try
+            {
+
+                var i = await storageFolder.GetFilesAsync();
+                var files = from x in i where x.DisplayName.Contains("_DEBUG") select x.DisplayName;
+                return files.ToList();
+
+            }
+            catch (Exception)
+            {
+
+                Debug.WriteLine("Error");
+                return null;
+            }
+
+
+
+        }
+        private async Task<bool> CalculateStusScoreAsync()
+        {
+
             StorageFile file = await storageFolder.CreateFileAsync(App.XmlFileName, CreationCollisionOption.OpenIfExists);
             XElement xEle;
-            using (var stream = await file.OpenStreamForReadAsync())
+            try
             {
-                xEle = XElement.Load(stream);
-            }
-            // System.Diagnostics.Debug.WriteLine(xEle);
-            var i = from x in xEle.Elements() select x.Attribute("missId").Value;
-            List<int> list = new List<int>();
-            foreach (var item in i)
-            {
-                string[] s = item.Split(',');
-                foreach (var t in s)
+                using (var stream = await file.OpenStreamForReadAsync())
                 {
-                    list.Add(int.Parse(t));
+                    xEle = XElement.Load(stream);
                 }
+                var i = from x in xEle.Elements() select x.Attribute("missId").Value;
+
+
+                List<int> list = new List<int>();
+                foreach (var item in i)
+                {
+                    string[] s = item.Split(',');
+                    foreach (var t in s)
+                    {
+                        list.Add(int.Parse(t));
+                    }
+                }
+                foreach (var item in list)
+                {
+                    //System.Diagnostics.Debug.WriteLine();
+                    App.Stus.ElementAt(item - 1).Score -= 1;
+                    //System.Diagnostics.Debug.WriteLine(App.Stus.ElementAt(item - 1).Name);
+                    //System.Diagnostics.Debug.WriteLine(App.Stus.ElementAt(item - 1).Score);
+                }
+                //System.Diagnostics.Debug.WriteLine("Finished");
+
             }
-            foreach (var item in list)
+            catch (Exception)
             {
-                //System.Diagnostics.Debug.WriteLine();
-                App.Stus.ElementAt(item - 1).Score -= 1;
-                //System.Diagnostics.Debug.WriteLine(App.Stus.ElementAt(item - 1).Name);
-                //System.Diagnostics.Debug.WriteLine(App.Stus.ElementAt(item - 1).Score);
+                return false;
             }
-            //System.Diagnostics.Debug.WriteLine("Finished");
+            return true;
+            // System.Diagnostics.Debug.WriteLine(xEle);
+
         }
+
         private async void ShowGridViewItemOfStusAsync(IEnumerable<Student> stus)
         {
-            await CalculateStusScoreAsync();
+            if (await CalculateStusScoreAsync())
+            {
+                TxtWrong.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                TxtWrong.Visibility = Visibility.Visible;
+                return;
+            }
             foreach (var item in stus)
-            { TextBlock block = new TextBlock() { Text = item.Name + " " + item.Score.ToString(), FontSize = 20 };
+            {
+                TextBlock block = new TextBlock() { Text = item.Name + " " + item.Score.ToString(), FontSize = 20 };
                 GridViewItem gvItem = new GridViewItem()
                 {
-                    Content =block,
-                    Width=100
+                    Content = block,
+                    Width = 100
                 };
                 //System.Diagnostics.Debug.WriteLine(item.Name);
                 //System.Diagnostics.Debug.WriteLine(item.Score);
                 Gv.Items.Add(gvItem);
             }
             //System.Diagnostics.Debug.WriteLine("Show Finish");
+        }
+
+        private void BtnChangeSeat_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void SaveStudents()
+        {
+            XDocument xDoc = new XDocument(
+                   new XElement(
+                    "students"
+)
+                                             );
+            foreach (var item in App.Stus)
+            {
+                xDoc.Element("students").Add(new XElement("student", new XAttribute("ID", item.Id), new XAttribute("CheckType", item.CType)));
+            }
+            StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+            //StorageFile file = await storageFolder.CreateFileAsync("temp.xml", CreationCollisionOption.ReplaceExisting);
+            //using (var stream = await file.OpenStreamForWriteAsync())
+            //{
+            //    xDoc.Save(stream);
+            //}
+
+
         }
     }
 }
